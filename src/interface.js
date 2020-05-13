c6ef46175f7732665c30825c32d79a688f026556
dpwdec <declankolakowski@gmail.com>
2020-05-13 17:04:51 +0100

    added jQuery functionality to up and down buttons.

---
+++ src/interface.js
@@ -0,0 +1,14 @@
+// javascript code goes here
+$(document).ready(function(){
+  thermostat = new Thermostat();
+  //jQuery code goes here
+  $('#temperature-up').on('click', function() {
+    thermostat.up(1);
+    console.log(thermostat.temperature);
+  });
+
+  $('#temperature-down').on('click', function() {
+    thermostat.down(1);
+    console.log(thermostat.temperature);
+  });
+});
