7007a2b0cd9960db8ca114ab924cd8dd6df9e9b8
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-13 17:20:51 +0100

    Power saving mode button functionality

--- src/interface.js
+++ src/interface.js
@@ -4,16 +4,35 @@ $(document).ready(function(){
   //jQuery code goes here
   $('#temperature-up').on('click', function() {
     thermostat.up(1);
-    console.log(thermostat.temperature);
+    $('#temperature').text(thermostat.temperature);
   });
 
   $('#temperature-down').on('click', function() {
     thermostat.down(1);
-    console.log(thermostat.temperature);
+    $('#temperature').text(thermostat.temperature);
   });
 
   $('#temperature-reset').on('click', function() {
     thermostat.reset();
-    console.log(thermostat.temperature);
+    $('#temperature').text(thermostat.temperature);
   });
+
+  $('#temperature').text(thermostat.temperature);
+
+  $('#powersaving-toggle').on('click', function(){
+    thermostat.togglePowerSavingMode();
+
+    if (thermostat.powerSavingMode === true){
+      $('#power-saving-status').text('on');
+    } else {
+      $('#power-saving-status').text('off');
+    }
+
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  // $('window').click(function(){
+  //   $('#temperature').text(thermostat.temperature);
+  // });
+
 });
